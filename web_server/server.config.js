1c95a2ad6980d3f876df77ccaef8fa8337fe7209
Ricardo Junior <ricardo@ricardo.lan>
2024-12-09 13:59:22 -0300

    att

--- web_server/server.config.js
+++ web_server/server.config.js
@@ -3,19 +3,11 @@ var fs = require('fs');
 var io = require('socket.io');
 var net = require('net');
 
-var usuarios = [];
-var ultimas_mensagens = [];
 const PORTA_HTTP = 3000;
 const PORTA_TCP = 4000; // Porta para o servidor TCP
 
-var app = http.createServer(resposta);
-io = io(app);
-
-app.listen(PORTA_HTTP);
-console.log("Servidor HTTP está em execução na porta " + PORTA_HTTP);
-
-// Função principal de resposta as requisições do servidor HTTP
-function resposta(req, res) {
+// Servidor HTTP simples
+var app = http.createServer(function(req, res) {
     var arquivo = "";
     if (req.url == "/") {
         arquivo = __dirname + '/index.html';
@@ -30,23 +22,23 @@ function resposta(req, res) {
         res.writeHead(200);
         res.end(data);
     });
-}
+});
 
-// Configuração do servidor TCP
+io = io(app);
+app.listen(PORTA_HTTP);
+console.log("Servidor HTTP está em execução na porta " + PORTA_HTTP);
+
+// Servidor TCP para receber mensagens do servidor Java
 var tcpServer = net.createServer(function (socket) {
     console.log('Conexão TCP recebida de ' + socket.remoteAddress + ':' + socket.remotePort);
-    usuarios.push({
-        endereco: socket.remoteAddress,
-        porta: socket.remotePort
-    });
-    
+
     socket.on('data', function (data) {
         var mensagem = data.toString();
         console.log('Mensagem recebida do servidor Java: ' + mensagem);
 
         var obj_mensagem = { msg: mensagem, tipo: 'sistema' };
+        // Emite para todos os clientes conectados via socket.io
         io.sockets.emit("atualizar mensagens", obj_mensagem);
-        armazenaMensagem(obj_mensagem);
     });
 
     socket.on('end', function () {
@@ -62,78 +54,5 @@ tcpServer.listen(PORTA_TCP, function () {
     console.log('Servidor TCP está em execução na porta ' + PORTA_TCP);
 });
 
-io.on("connection", function (socket) {
-    socket.on("entrar", function (apelido, callback) {
-        if (!(apelido in usuarios)) {
-            socket.apelido = apelido;
-            usuarios[apelido] = socket;
-
-            for (var indice in ultimas_mensagens) {
-                socket.emit("atualizar mensagens", ultimas_mensagens[indice]);
-            }
-
-            var mensagem = "[ " + pegarDataAtual() + " ] " + apelido + " acabou de entrar na sala";
-            var obj_mensagem = { msg: mensagem, tipo: 'sistema' };
-
-            io.sockets.emit("atualizar usuarios", Object.keys(usuarios));
-            io.sockets.emit("atualizar mensagens", obj_mensagem);
-
-            armazenaMensagem(obj_mensagem);
-
-            callback(true);
-        } else {
-            callback(false);
-        }
-    });
-
-    socket.on("enviar mensagem", function (dados, callback) {
-        var mensagem_enviada = dados.msg;
-        var usuario = dados.usu;
-        if (usuario == null)
-            usuario = '';
-
-        mensagem_enviada = "[ " + pegarDataAtual() + " ] " + socket.apelido + " diz: " + mensagem_enviada;
-        var obj_mensagem = { msg: mensagem_enviada, tipo: '' };
-
-        if (usuario == '') {
-            io.sockets.emit("atualizar mensagens", obj_mensagem);
-            armazenaMensagem(obj_mensagem);
-        } else {
-            obj_mensagem.tipo = 'privada';
-            socket.emit("atualizar mensagens", obj_mensagem);
-            usuarios[usuario].emit("atualizar mensagens", obj_mensagem);
-        }
-        callback();
-    });
-
-    socket.on("disconnect", function () {
-        delete usuarios[socket.apelido];
-        var mensagem = "[ " + pegarDataAtual() + " ] " + socket.apelido + " saiu da sala";
-        var obj_mensagem = { msg: mensagem, tipo: 'sistema' };
-
-        io.sockets.emit("atualizar usuarios", Object.keys(usuarios));
-        io.sockets.emit("atualizar mensagens", obj_mensagem);
-
-        armazenaMensagem(obj_mensagem);
-    });
-});
-
-function pegarDataAtual() {
-    var dataAtual = new Date();
-    var dia = (dataAtual.getDate() < 10 ? '0' : '') + dataAtual.getDate();
-    var mes = ((dataAtual.getMonth() + 1) < 10 ? '0' : '') + (dataAtual.getMonth() + 1);
-    var ano = dataAtual.getFullYear();
-    var hora = (dataAtual.getHours() < 10 ? '0' : '') + dataAtual.getHours();
-    var minuto = (dataAtual.getMinutes() < 10 ? '0' : '') + dataAtual.getMinutes();
-    var segundo = (dataAtual.getSeconds() < 10 ? '0' : '') + dataAtual.getSeconds();
-
-    var dataFormatada = dia + "/" + mes + "/" + ano + " " + hora + ":" + minuto + ":" + segundo;
-    return dataFormatada;
-}
-
-function armazenaMensagem(mensagem) {
-    if (ultimas_mensagens.length > 5) {
-        ultimas_mensagens.shift();
-    }
-    ultimas_mensagens.push(mensagem);
-}
+// Não há mais lógica para armazenamento de usuários ou mensagens anteriores.
+// Cada conexão TCP é usada apenas para transportar a mensagem recebida do servidor Java até o cliente via socket.io.
